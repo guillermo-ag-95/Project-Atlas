c69f4919e83317280a20d2fbf1e27e9f267e9b54
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2017-11-28 14:22:17 +0100

    First steps to get the velocity from acceleration

--- iPhone Accelerometer Gauge/iPhone Accelerometer Gauge/MainViewController.swift
+++ iPhone Accelerometer Gauge/iPhone Accelerometer Gauge/MainViewController.swift
@@ -13,11 +13,15 @@ import AVFoundation
 class MainViewController: UIViewController {
     
     var motionManager = CMMotionManager()
+    
     var accelerometerXData: [Double] = []
     var accelerometerYData: [Double] = []
     var accelerometerZData: [Double] = []
     
-    let systemSoundID: SystemSoundID = 1052 // SIMToolkitGeneralBeep.caf
+    var velocityXData: [Double] = [0]
+    var velocityYData: [Double] = [0]
+    var velocityZData: [Double] = [0]
+    
     let updatesIntervalOn = 0.01
     let updatesIntervalOff = 0.1
     let gravity = 9.81
@@ -30,8 +34,8 @@ class MainViewController: UIViewController {
     @IBOutlet weak var playButton: UIButton!
     @IBOutlet weak var pauseButton: UIButton!
     
-    @IBOutlet weak var accelerationTextLabel: UILabel!
-    @IBOutlet weak var accelerationValueLabel: UILabel!
+    @IBOutlet weak var speedTextLabel: UILabel!
+    @IBOutlet weak var speedValueLabel: UILabel!
     @IBOutlet weak var xAxisTextLabel: UILabel!
     @IBOutlet weak var yAxisTextLabel: UILabel!
     @IBOutlet weak var zAxisTextLabel: UILabel!
@@ -46,16 +50,14 @@ class MainViewController: UIViewController {
         
         // Updates the interval to avoid 100Hz when the app is paused.
         self.motionManager.deviceMotionUpdateInterval = playButton.isHidden ? self.updatesIntervalOn : self.updatesIntervalOff
-        
+
         playButton.isHidden ? startRecordData() : stopRecordData()
     }
     
     func startRecordData(){
         guard motionManager.isDeviceMotionAvailable else { return }
         
-        accelerometerXData.removeAll()
-        accelerometerYData.removeAll()
-        accelerometerZData.removeAll()
+        initializeStoredData()
         
         motionManager.startDeviceMotionUpdates(to: OperationQueue.current!) { (data, error) in
             if let data = data {
@@ -70,35 +72,60 @@ class MainViewController: UIViewController {
         motionManager.stopDeviceMotionUpdates()
     }
     
+    func initializeStoredData(){
+        accelerometerXData.removeAll()
+        accelerometerYData.removeAll()
+        accelerometerZData.removeAll()
+        
+        velocityXData.removeAll()
+        velocityXData.append(0)
+        velocityYData.removeAll()
+        velocityYData.append(0)
+        velocityZData.removeAll()
+        velocityZData.append(0)
+    }
+    
     func updateStoredData(_ data: CMDeviceMotion){
         accelerometerXData.append(data.userAcceleration.x * gravity)
         accelerometerYData.append(data.userAcceleration.y * gravity)
         accelerometerZData.append(data.userAcceleration.z * gravity)
+        
+        // Vi = Vi-1 + ai*t
+        let newXVelocity = velocityXData.last! + accelerometerXData.last! * updatesIntervalOn
+        let newYVelocity = velocityYData.last! + accelerometerYData.last! * updatesIntervalOn
+        let newZVelocity = velocityZData.last! + accelerometerZData.last! * updatesIntervalOn
+
+        velocityXData.append(newXVelocity)
+        velocityYData.append(newYVelocity)
+        velocityZData.append(newZVelocity)
     }
     
     func initializeInterface(){
         playButton.isHidden = false
         pauseButton.isHidden = true
         
-        accelerationTextLabel.text = "Acceleration"
+        speedTextLabel.text = "Speed"
         xAxisTextLabel.text = "X Axis:"
         yAxisTextLabel.text = "Y Axis:"
         zAxisTextLabel.text = "Z Axis:"
         
-        accelerationValueLabel.text = nil
+        speedValueLabel.text = nil
         xAxisValueLabel.text = nil
         yAxisValueLabel.text = nil
         zAxisValueLabel.text = nil
     }
     
     func updateLabels(){
-        self.xAxisValueLabel.text = String(accelerometerXData.last!)
-        self.yAxisValueLabel.text = String(accelerometerYData.last!)
-        self.zAxisValueLabel.text = String(accelerometerZData.last!)
+        self.xAxisValueLabel.text = String(format: "%.2f", arguments: [accelerometerXData.last!])
+        self.yAxisValueLabel.text = String(format: "%.2f", arguments: [accelerometerYData.last!])
+        self.zAxisValueLabel.text = String(format: "%.2f", arguments: [accelerometerZData.last!])
+        
+        let velocity = sqrt(pow(velocityXData.last!, 2) + pow(velocityYData.last!, 2) + pow(velocityZData.last!, 2))
+        self.speedValueLabel.text = String(format: "%.2f", arguments: [velocity])
     }
     
     func playSound(){
-        AudioServicesPlaySystemSound(systemSoundID)
+        AudioServicesPlaySystemSound(1052) // SIMToolkitGeneralBeep.caf
     }
     
 }
