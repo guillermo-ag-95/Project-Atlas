631ff593c83d37026aad04279661d2d4248ccd75
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-03-31 13:06:21 +0200

    Added simple Filter.swift file to check tests
    This file contains a basic sum function. This function will be used to check that Unit Tests work well.

---
+++ Speed Gauge/Speed Gauge/Model/Filter.swift
@@ -0,0 +1,17 @@
+//
+//  Filter.swift
+//  Speed Gauge
+//
+//  Created by Guillermo Alcalá Gamero on 31/3/18.
+//  Copyright © 2018 Guillermo Alcalá Gamero. All rights reserved.
+//
+
+import Foundation
+import Surge
+
+class Filter {
+    
+    func sum(first: Int, last: Int) -> Int {
+        return first + last;
+    }
+}
