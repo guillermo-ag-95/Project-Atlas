24db271b27ef1f23c40b4c15a18ad4c0fd2fa6ec
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-04-02 15:53:51 +0200

    Unorthodox Kalman Filter test
    The Kalman Filter test is not a call to a function. Instead, it's the full procedure to make it work.
    
    Gaussian tests have been redone with better variable names and data that reflects an actual Kalman Filter.

--- Speed Gauge/Speed Gauge Tests/One_Dimensional_Kalman_Filter_Tests.swift
+++ Speed Gauge/Speed Gauge Tests/One_Dimensional_Kalman_Filter_Tests.swift
@@ -14,23 +14,45 @@ class One_Dimensional_Kalman_Filter_Tests: Abstract_Test {
     let one_Dimensional_Kalman_Filter_Tests = One_Dimensional_Kalman_Filter()
     
     func test_gaussian_1(){
-        let gaussian_1 = Gaussian(mean: 10.0, variance: 0.2*0.2)
-        let gaussian_2 = Gaussian(mean: 15.0, variance: 0.7*0.7)
+        let position = Gaussian(mean: 10.0, variance: 0.2*0.2)
+        let velocity = Gaussian(mean: 15.0, variance: 0.7*0.7)
         
-        let gaussian_3 = Gaussian.predict(lhs: gaussian_1, rhs: gaussian_2)
+        let prior = Gaussian.predict(lhs: position, rhs: velocity)
         
-        XCTAssertEqual(gaussian_3.mean, 25.0, accuracy: 0.001)
-        XCTAssertEqual(gaussian_3.variance, 0.53, accuracy: 0.001)
+        XCTAssertEqual(prior.mean, 25.0, accuracy: 0.001)
+        XCTAssertEqual(prior.variance, 0.53, accuracy: 0.001)
     }
     
     func test_gaussian_2(){
-        let gaussian_1 = Gaussian(mean: 10.0, variance: 0.2*0.2)
-        let gaussian_2 = Gaussian(mean: 15.0, variance: 0.7*0.7)
+        let prior = Gaussian(mean: 25.0, variance: 0.53)
+        let likehood = Gaussian(mean: 23.0, variance: 0.16*0.16)
         
-        let gaussian_3 = Gaussian.update(lhs: gaussian_1, rhs: gaussian_2)
+        let posterior = Gaussian.update(lhs: prior, rhs: likehood)
         
-        XCTAssertEqual(gaussian_3.mean, 10.377358490566, accuracy: 0.001)
-        XCTAssertEqual(gaussian_3.variance, 0.0369811320754717, accuracy: 0.001)
+        XCTAssertEqual(posterior.mean, 23.0921526278, accuracy: 0.001)
+        XCTAssertEqual(posterior.variance, 0.02442044636, accuracy: 0.001)
+    }
+    
+    func test_fist_kalman_filter_1(){
+        let process_var = 1.0 // Variance of the movement.
+        let sensor_var = 2.0 // Variance of the sensor.
+        
+        var x = Gaussian(mean: 0.0, variance: 20.0*20.0) // Initial position.
+        
+        let velocity = 1
+        let dt = 1.0 // Time steps in seconds.
+        let process_model = Gaussian(mean: Double(velocity)*dt, variance: process_var)
+        
+        let zs = [1.354, 1.882, 4.341, 7.156, 6.939, 6.844, 9.847, 12.553, 16.273, 14.800]
+        
+        // Perform Kalman filter on measurements z
+        for z in zs {
+            let prior = Gaussian.predict(lhs: x, rhs: process_model)
+            let likehood = Gaussian(mean: z, variance: sensor_var)
+            x = Gaussian.update(lhs: prior, rhs: likehood)
+        }
+        
+        XCTAssertEqual(x.mean, 15.053, accuracy: 0.1)
     }
 
 }
