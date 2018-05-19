b16f9286dd901c26940f9e6eaf35d5427b0b52bc
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-05-19 18:18:52 +0200

    You can now delete a repetition

--- Speed Gauge/Speed Gauge/RepetitionTableViewController.swift
+++ Speed Gauge/Speed Gauge/RepetitionTableViewController.swift
@@ -59,17 +59,18 @@ class RepetitionTableViewController: UITableViewController {
     }
     */
 
-    /*
     // Override to support editing the table view.
     override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
         if editingStyle == .delete {
+            maxVelocities.remove(at: indexPath.section)
+            meanVelocities.remove(at: indexPath.section)
             // Delete the row from the data source
-            tableView.deleteRows(at: [indexPath], with: .fade)
+            // tableView.deleteRows(at: [indexPath], with: .fade)
+            tableView.reloadData()
         } else if editingStyle == .insert {
             // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
         }    
     }
-    */
 
     /*
     // Override to support rearranging the table view.
