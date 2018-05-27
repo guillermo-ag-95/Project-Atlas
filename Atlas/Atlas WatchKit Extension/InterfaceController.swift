fdbb7f3668104842e96ce6b41e1b79f9f95cec86
Guillermo Alcalá Gamero <guillermo.ag.95@gmail.com>
2018-05-27 12:01:30 +0200

    Added remaining functions
    - dot and mean function have been coded.
    - Another timer has been added to avoid malfunction when you change the delay before the timer ends.
    - Added gravity variable to get results in meters per seconds (and not in G).

--- Atlas/Atlas WatchKit Extension/InterfaceController.swift
+++ Atlas/Atlas WatchKit Extension/InterfaceController.swift
@@ -23,14 +23,16 @@ class InterfaceController: WKInterfaceController {
     var buttonVisibility: Bool = true                           // Boolean to alternate between play and pause button.
     var delay: Int = 0                                          // Delay to start the accelerometer measurements.
     var motionManager: CMMotionManager?                         // Motion Manager that will retrieve the accelerometer information.
-    var timer : Timer?                                          // Timer that will delay the start of the accelerometer measures.
-    
+    var timerCount: Timer?                                      // Timer that will delay the start of the accelerometer measures.
+    var timerAccelerometer: Timer?                              // Timer that will delay the start of the accelerometer measures.
+
     var maxVelocities: [Double] = []                            // Array with the max velocity of every rep.
     var meanVelocities: [Double] = []                           // Array with the mean velocity of every rep.
     var verticalAcceleration: [Double] = []                     // Array of vertical accelerations.
     var verticalVelocity: [Double] = []                         // Array of vertical velocities.
     var verticalFixedVelocity: [Double] = []                    // Array of vertical velocities after the data treatment.
     
+    let gravity = 9.81                                          // Gravity conversion from G to m/s.
     let queue: OperationQueue = OperationQueue()                // OperationQueue to execute the accelerometer updates.
     let times: [Int] = [0,1,3,5,7,10,12,15,17,20,23,25,30]      // Array of possible delays to choose from.
     let updateIntervalOn = 0.01                                 // Update interval of 0.01 seconds (100 Hz)
@@ -46,9 +48,10 @@ class InterfaceController: WKInterfaceController {
         // Configure time picker.
         timePicker.setItems(initializePicker(times))
         
-        // Configure timer.
-        timer = Timer.init()
-        
+        // Configure timers.
+        timerCount = Timer.init()
+        timerAccelerometer = Timer.init()
+
         // Configure audio player.
         audioPlayer = initializeAudioPlayer()
         
@@ -77,9 +80,10 @@ class InterfaceController: WKInterfaceController {
         // Only trigger the action after the play button is pressed.
         if buttonTimesPressed % 2 == 1 {
             // Delay the start of the accelerometer updates.
-            timer?.invalidate()
-            timer = Timer.scheduledTimer(timeInterval: Double(delay), target: self, selector: #selector(self.playSound), userInfo: nil, repeats: false)
-            timer = Timer.scheduledTimer(timeInterval: Double(delay), target: self, selector: #selector(self.startRecordData), userInfo: nil, repeats: false)
+            timerCount?.invalidate()
+            timerAccelerometer?.invalidate()
+            timerCount = Timer.scheduledTimer(timeInterval: Double(delay), target: self, selector: #selector(self.playSound), userInfo: nil, repeats: false)
+            timerAccelerometer = Timer.scheduledTimer(timeInterval: Double(delay), target: self, selector: #selector(self.startRecordData), userInfo: nil, repeats: false)
         } else {
             stopRecordData()
         }
@@ -111,7 +115,7 @@ class InterfaceController: WKInterfaceController {
         delay = times[value]
     }
     
-    // Ancillary functions:
+    // Support functions:
     
     func initializePicker(_ times: [Int]) -> [WKPickerItem] {
         var timePickerItems: [WKPickerItem] = []
@@ -155,12 +159,13 @@ class InterfaceController: WKInterfaceController {
         
         // Compute scalar projection of the acceleration vector onto the gravity vector.
         let gravityModule = sqrt(pow(newXGravity, 2) + pow(newYGravity, 2) + pow(newZGravity, 2))
-        let accelerometerData = [newXAcceleration, newYAcceleration, newZAcceleration]
-        let gravityData = [newXGravity, newYGravity, newZGravity]
-        let scalarProjection = 0.0 // TODO with Surge.dot
+        let accelerationVector = [newXAcceleration, newYAcceleration, newZAcceleration]
+        let gravityVector = [newXGravity, newYGravity, newZGravity]
+        let dotProduct = dot(gravityVector, accelerationVector)
+        let scalarProjection = gravityVector.map { dotProduct / pow(gravityModule, 2) * $0 * self.gravity }
         
         // Compute vertical acceleration and velocity.
-        let newVerticalAcceleration = 0.0 // TODO with Surge.dot
+        let newVerticalAcceleration = sign(dotProduct) * sqrt(pow(scalarProjection[0], 2) + pow(scalarProjection[1], 2) + pow(scalarProjection[2], 2))
         let newVerticalVelocity = (verticalAcceleration.last! * updateIntervalOn) + (newVerticalAcceleration - verticalAcceleration.last!) * (updateIntervalOn / 2)
         let currentVerticalVelocity = verticalVelocity.last! + newVerticalVelocity
         
@@ -207,13 +212,37 @@ class InterfaceController: WKInterfaceController {
                     maximum = 0.0                   // Reset the maximum velocity.
                     
                     let repVelocities = verticalFixedVelocity.suffix(from: startingPoints.last!).prefix(upTo: endingPoints.last!)
-                    let meanVelocity = 0.0 // TODO: Surge.mean(Array(repVelocities))
+                    let meanVelocity = mean(Array(repVelocities))
                     
                     meanVelocities.append(meanVelocity)
                 }
             }
         }
     }
+    
+    // Math functions
+    
+    func dot(_ x: [Double], _ y: [Double]) -> Double {
+        assert(x.count == y.count, "Both vectors must be the same size")
+        
+        var result = 0.0
+        
+        for i in 0..<x.count {
+            result = result + x[i] * y[i]
+        }
+        return result
+    }
+    
+    func mean(_ x: [Double]) -> Double {
+        var result = 0.0
+        
+        result = x.reduce(0.0, +)
+        result = result / Double(x.count)
+        
+        return result
+    }
+    
+    // Ancillary functions:
     
     @objc func playSound(){
         do {
@@ -228,4 +257,3 @@ class InterfaceController: WKInterfaceController {
     }
     
 }
-// Compute scalar projection of the acceleration vector onto the gravity vector
